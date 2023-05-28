af1f7c05679085018665efaa603090e717cac06d
Niyaz Fazlyev <>
2023-05-28 22:13:41 +0300

    lecture 7

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -38,23 +38,19 @@ struct CardView: View {
     let card: MemoryGame<String>.Card
     
     var body: some View {
+        
         GeometryReader{ geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Pie(
-                        startAngle: Angle(degrees: -90),
-                        endAngle: Angle(degrees: -180 )
+                  Pie(
+                    startAngle: Angle(degrees: -90),
+                    endAngle: Angle(degrees: -180 )
                     ).padding(4).opacity(0.5)
-                    Text(card.content).font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
-                }
-            }
+                    Text(card.content)
+                    .rotationEffect(Angle(degrees: card.isMatched ? 360 : 0 ))
+                    .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
+            }.cardify(isFaceUp: card.isFaceUp)
         }
         
     }
@@ -62,12 +58,14 @@ struct CardView: View {
     private func font(in size: CGSize) -> Font {
         Font.system(size: DrawingConstants.fontScale * min(size.height, size.width))
     }
+    
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.height, size.width) / DrawingConstants.fontSize * DrawingConstants.fontScale
+    }
 
     private struct DrawingConstants {
-        static let cornerRadius:CGFloat = 10
-        static let lineWidth:CGFloat = 3
-        static let fontScale:CGFloat = 0.7
-        
+        static let fontScale: CGFloat = 0.7
+        static let fontSize: CGFloat = 32
     }
 }
 
